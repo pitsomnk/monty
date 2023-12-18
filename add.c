13a683058174fd0163c09777d6b76a82ceb7c372
pitsomnk <pitsomnk@gmail.com>
2023-12-18 12:00:29 -0800

    another commit

--- add.c
+++ add.c
@@ -1,27 +1,20 @@
 #include "monty.h"
-void f_add(stack_t **head, unsigned int counter)
+void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *h;
-	int len = 0, aux;
+	stack_t *first_ele, *second_ele;
 
-	h = *head;
-	while (h)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		release(NULL, NULL, 'r');
+		free_dlistint(*stack);
+		free(global.line);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	aux = h->n + h->next->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+	first_ele = *stack;
+	second_ele = (*stack)->next;
+	second_ele->n += first_ele->n;
+	*stack = second_ele;
+	second_ele->prev = NULL;
+	free(first_ele);
 }
-
